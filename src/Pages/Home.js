d8f680ee0a04537b07e8787671763b212a3d26a0
dani7beth <danielledona28@gmail.com>
2021-02-26 16:51:59 -0700

    added communication from button to script

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,19 +3,27 @@ import Script from '../script.js';
 
 export const Home = () =>{
   const script = new Script ();
+  script.handler();
 
+  const renderLog = () =>{
+    return script.msgLog.map((item) =>{
+      return(
+        <p>{item}</p>
+      )
+    })
+  }
   return(
     <>
     <h1>AutoMod Bot</h1>
     <Container>
       <div>
         <h3>Control Panel</h3>
-        <button>Start</button>
-        <button>Stop</button>
+        <button onClick={() => script.connection()}>Start</button>
+        <button onClick={()=> script.disconnect()}>Stop</button>
       </div>
       <div>
         <h3>Log</h3>
-        <p>card</p>
+        {renderLog()}
       </div>
     </Container>
     </>
@@ -25,4 +33,5 @@ export const Container = styled.div`
 display: flex;
 flex-direction: row;
 justify-content: space-between;
+padding: 50px;
 `;
--- src/script.js
+++ src/script.js
@@ -1,46 +1,62 @@
 const tmi = require("tmi.js");
-require("dotenv");
-const log = [];
+class Script {
+  constructor() {
+    this.msgLog = [];
+    this.opts = {
+      connection: {
+        secure: true,
+      },
+      identity: {
+        username: process.env.REACT_APP_USER,
+        password: process.env.REACT_APP_OATH_CODE,
+      },
+      channels: [process.env.REACT_APP_CHANNEL],
+    };
+    this.client = new tmi.client(this.opts);
+  }
 
-const opts = {
-  identity: {
-    username: process.env.USER,
-    password: process.env.OATH_CODE,
-  },
-  channels: [process.env.CHANNEL],
-};
+  //connect to twitch
+  connection() {
+    this.client.connect().catch(console.error);
+  }
+  disconnect() {
+    this.client.disconnect();
+  }
+  
+  //handler functions
+  handleConnect(addr) {
+    console.log(this.msgLog)
+    console.log(`Connected to ${addr}`);
+    // this.msgLog.push(`Connected to ${addr}`);
+    if (this.msgLog) {
+      this.msgLog.push("Connected to the server");
+    }
+  }
 
-const client = new tmi.client(opts);
+  handleRaid(channel, username, viewers) {
+    console.log(`${username} raided the channel with ${viewers} viewers!`);
+    this.msgLog.push(`${username} raided the channel with ${viewers} viewers!`);
+    // setTimeout(() => {
+    //   client.say(channel, `!so @${username}`);
+    // }, Math.floor(Math.random() * 5000));
+  }
 
-//connect to twitch
-client.connect();
+  handleSub(channel, username) {
+    console.log(`${username} just subbed!`);
+    this.msgLog.push(`${username} just subbed!`);
+    // setTimeout(() => {
+    //   client.say(channel, "!sh");
+    // }, Math.floor(Math.random() * 5000));
+  }
 
-//handler functions
-const handleConnect = (addr) => {
-  console.log(`Connected to ${addr}`);
-  log.push(`Connected to ${addr}`);
-};
-
-const handleRaid = (channel, username, viewers) => {
-  console.log(`${username} raided the channel with ${viewers} viewers!`);
-  log.push(`${username} raided the channel with ${viewers} viewers!`);
-  setTimeout(() => {
-    client.say(channel, `!so @${username}`);
-  }, Math.floor(Math.random() * 5000));
-};
-
-const handleSub = (channel, username) => {
-  console.log(`${username} just subbed!`);
-  log.push(`${username} just subbed!`)
-  // setTimeout(() => {
-  //   client.say(channel, "!sh");
-  // }, Math.floor(Math.random() * 5000));
-};
-
-//event handlers
-client.on("connected", handleConnect);
-client.on("raided", handleRaid);
-client.on("resub", handleSub);
-client.on("subgift", handleSub);
-client.on("submysterygift", handleSub);
-client.on("subscription", handleSub);
+  //event handlers
+  handler() {
+    this.client.on("connected", this.handleConnect);
+    this.client.on("raided", this.handleRaid);
+    this.client.on("resub", this.handleSub);
+    this.client.on("subgift", this.handleSub);
+    this.client.on("submysterygift", this.handleSub);
+    this.client.on("subscription", this.handleSub);
+  }
+}
+export default Script;
